d20b18a8db06748d9950f40b82f2254bf0bf9ac4
aubierr <74046434+aubierr@users.noreply.github.com>
2024-02-12 17:34:37 +0100

    Update api.py

--- custom_components/aircloud/api.py
+++ custom_components/aircloud/api.py
@@ -1,11 +1,11 @@
-import requests
-import uuid
+import aiohttp
 import json
 import logging
-
+import uuid
 from datetime import datetime
-from websocket import create_connection
-from .const import API_HOST, AUTH_URN, WHO_URN, WSS_URL, CONTROL_URN, REFRESH_TOKEN_URN
+from aiohttp import WSMsgType
+
+from .const import HOST_API, URN_AUTH, URN_WHO, URN_WSS, URN_CONTROL, URN_REFRESH_TOKEN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,61 +20,80 @@ class AirCloudApi:
         self._family_id = None
         self._data = None
         self._ref_token = None
+        self._session = aiohttp.ClientSession()
 
-    def __authenticate(self):
+    async def validate_credentials(self):
+        try:
+            await self.__authenticate()
+            return True
+        except Exception as e:
+            logging.error("Failed to validate credentials: %s", str(e))
+            return False
+
+    async def __authenticate(self):
         authorization = {"email": self._login, "password": self._password}
-        self.__update_token_data(requests.post(API_HOST + AUTH_URN, json = authorization))
+        async with self._session.post(HOST_API + URN_AUTH, json=authorization) as response:
+            await self.__update_token_data(await response.json())
         self._last_token_update = datetime.now()
         if self._family_id is None:
-            self.__load_family_id()
+            await self.__load_family_id()
 
-    def __refresh_token(self):
+    async def __refresh_token(self):
         now_datetime = datetime.now()
         td = now_datetime - self._last_token_update
         td_minutes = divmod(td.total_seconds(), 60)
 
         if self._token is None:
-            self.__authenticate()
+            await self.__authenticate()
         elif td_minutes[1] > 5:
-            self.__update_token_data(requests.post(API_HOST + REFRESH_TOKEN_URN, headers = {"Authorization": "Bearer " + self._ref_token, "isRefreshToken": "true"}))
+            async with self._session.post(HOST_API + URN_REFRESH_TOKEN,
+                                          headers={"Authorization": "Bearer " + self._ref_token,
+                                                    "isRefreshToken": "true"}) as response:
+                await self.__update_token_data(await response.json())
             self._last_token_update = now_datetime
 
-    def __update_token_data(self, response):
-         self._token = response.json()["token"]
-         self._ref_token = response.json()["refreshToken"]
+    async def __update_token_data(self, response):
+        self._token = response["token"]
+        self._ref_token = response["refreshToken"]
 
-    def __load_family_id(self):
-        response = requests.get(API_HOST + WHO_URN, headers =  self.__create_headers())
-        self._family_id = response.json()["familyId"]
+    async def __load_family_id(self):
+        async with self._session.get(HOST_API + URN_WHO, headers=self.__create_headers()) as response:
+            self._family_id = (await response.json())["familyId"]
 
-    def load_climate_data(self):
-        self.__refresh_token()
-        ws = create_connection(WSS_URL)
-        ws.send("CONNECT\naccept-version:1.1,1.2\nheart-beat:10000,10000\nAuthorization:Bearer " + self._token + "\n\n\0\nSUBSCRIBE\nid:" + str(uuid.uuid4()) + "\ndestination:/notification/" + str(self._family_id) + "/" + str(self._family_id) + "\nack:auto\n\n\0")
+    async def load_climate_data(self):
+        await self.__refresh_token()
+        async with self._session.ws_connect(URN_WSS) as ws:
+            await ws.send_str("CONNECT\naccept-version:1.1,1.2\nheart-beat:10000,10000\nAuthorization:Bearer " +
+                              self._token + "\n\n\0\nSUBSCRIBE\nid:" + str(uuid.uuid4()) + "\ndestination:/notification/"
+                              + str(self._family_id) + "/" + str(self._family_id) + "\nack:auto\n\n\0")
 
-        response = None
-        while response is None or "{" not in response:
-            response = ws.recv()
-        ws.close()
+            while True:
+                msg = await ws.receive()
+                if msg.type == WSMsgType.TEXT:
+                    response = msg.data
+                    if "{" in response:
+                        break
 
-        _LOGGER.debug("AirCloud climate data: " + str(response))
-        message = "{" + response.partition("{")[2].replace("\0", "")
-        struct = json.loads(message)
+            _LOGGER.debug("AirCloud climate data: " + str(response))
+            message = "{" + response.partition("{")[2].replace("\0", "")
+            struct = json.loads(message)
+            return struct["data"]
 
-        return struct["data"]
-    
-    def execute_command(self, id, power, idu_temperature, mode, fan_speed, fan_swing, humidity):
-        self.__refresh_token()
+    async def execute_command(self, id, power, idu_temperature, mode, fan_speed, fan_swing, humidity):
+        await self.__refresh_token()
         if mode == "FAN" and fan_speed == "AUTO":
             command = {"power": power, "iduTemperature": "0", "mode": mode, "fanSpeed": "LV2", "fanSwing": fan_swing, "humidity": humidity}
         elif mode == "FAN":
             command = {"power": power, "iduTemperature": "0", "mode": mode, "fanSpeed": fan_speed, "fanSwing": fan_swing, "humidity": humidity}
         else:
             command = {"power": power, "iduTemperature": idu_temperature, "mode": mode, "fanSpeed": fan_speed, "fanSwing": fan_swing, "humidity": humidity}
-        response = requests.put(API_HOST + CONTROL_URN + "/" + str(id) + "?familyId=" + str(self._family_id), headers=self.__create_headers(), json=command)
-        _LOGGER.debug("AirCloud command request: " + str(command))
-        _LOGGER.debug("AirCloud command response: " + str(response.content))
-     
+        async with self._session.put(HOST_API + URN_CONTROL + "/" + str(id) + "?familyId=" + str(self._family_id),
+                                     headers=self.__create_headers(), json=command) as response:
+            _LOGGER.debug("AirCloud command request: " + str(command))
+            _LOGGER.debug("AirCloud command response: " + str(await response.text()))
+
     def __create_headers(self):
-       return {"Authorization": "Bearer " + self._token}
-    
+        return {"Authorization": "Bearer " + self._token}
+
+    async def close_session(self):
+        await self._session.close()
