c8116f51eb8c911856f26a9115f8743ee5ece441
Ashish <ashish.devasia7@gmail.com>
2025-04-07 01:01:55 +0530

    Changes for debugging

--- custom_components/air_cloud/climate.py
+++ custom_components/air_cloud/climate.py
@@ -302,6 +302,7 @@ class AirCloudClimateEntity(ClimateEntity):
         await self.async_update()
 
     def __update_data(self, climate_data):
+        raise RuntimeError(f"AirCloud Test: {climate_data}")
         self._power = climate_data["power"]
         self._mode = climate_data["mode"]
         self._target_temp = climate_data["iduTemperature"]
